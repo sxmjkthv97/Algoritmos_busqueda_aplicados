07aadef9b70c7b87a4a3e00b38b9bb7563913708
sxmjkthv97 <est.samanthalucia.moreno.za@unifranz.edu.bo>
2026-09-09 09:04:29 -0400

    algoritmo de busqueda aplicado

--- unidimensional/busqueda_secuencial.php
+++ unidimensional/busqueda_secuencial.php
@@ -1,16 +1,4 @@
 <?php
-
-function busquedaSecuencial(array $arreglo, int $objetivo): int
-{
-    for ($i = 0; $i < count($arreglo); $i++) {
-        if ($arreglo[$i] === $objetivo) {
-            return $i;
-        }
-    }
-
-    return -1;
-}
-
 $arreglo = [10, 25, 8, 40, 15, 30];
 $objetivo = 40;
 
